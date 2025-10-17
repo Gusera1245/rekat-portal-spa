999772be7bd6566d8d9dfec339321917233ec954
Gusera1245 <149485541+Gusera1245@users.noreply.github.com>
2025-10-17 18:31:42 +0700

    Create firebase_config.js

---
+++ firebase_config.js
@@ -0,0 +1,15 @@
+// --- KONFIGURASI PROYEK FIREBASE DARI learnhub-app-92559 ---
+
+// Kunci publik yang diperlukan oleh aplikasi untuk terhubung ke Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyCIKKXwLPMnzYvoMSDAxZYoAz35vjbIKgQ",
+    authDomain: "learnhub-app-92559.firebaseapp.com",
+    projectId: "learnhub-app-92559",
+    storageBucket: "learnhub-app-92559.appspot.com", // Saya sesuaikan dengan format bucket yang benar
+    messagingSenderId: "525051273065",
+    appId: "1:525051273065:web:81f14a687e8632f3b7a7f8"
+    // measurementId: "G-XT0GQQSM17" <-- Tidak diperlukan untuk fungsi utama
+};
+
+// ID Proyek digunakan sebagai ID aplikasi di path Firestore/Storage
+export const CANVAS_APP_ID = "learnhub-app-92559";
