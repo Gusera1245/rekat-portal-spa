c06ff336c612e20011bba36c3e929c73dc953bbd
Gusera1245 <149485541+Gusera1245@users.noreply.github.com>
2025-10-18 19:01:19 +0700

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,15 +1,21 @@
-// --- KONFIGURASI PROYEK FIREBASE DARI learnhub-app-92559 ---
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Kunci publik yang diperlukan oleh aplikasi untuk terhubung ke Firebase
-export const firebaseConfig = {
-    apiKey: "AIzaSyCIKKXwLPMnzYvoMSDAxZYoAz35vjbIKgQ",
-    authDomain: "learnhub-app-92559.firebaseapp.com",
-    projectId: "learnhub-app-92559",
-    storageBucket: "learnhub-app-92559.appspot.com", // Saya sesuaikan dengan format bucket yang benar
-    messagingSenderId: "525051273065",
-    appId: "1:525051273065:web:81f14a687e8632f3b7a7f8"
-    // measurementId: "G-XT0GQQSM17" <-- Tidak diperlukan untuk fungsi utama
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIKKXwLPMnzYvoMSDAxZYoAz35vjbIKgQ",
+  authDomain: "learnhub-app-92559.firebaseapp.com",
+  projectId: "learnhub-app-92559",
+  storageBucket: "learnhub-app-92559.firebasestorage.app",
+  messagingSenderId: "525051273065",
+  appId: "1:525051273065:web:81f14a687e8632f3b7a7f8",
+  measurementId: "G-XT0GQQSM17"
 };
 
-// ID Proyek digunakan sebagai ID aplikasi di path Firestore/Storage
-export const CANVAS_APP_ID = "learnhub-app-92559";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
